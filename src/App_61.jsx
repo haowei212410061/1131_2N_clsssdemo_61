850b7c2a30b641492e17e8ddf1508f9c04647d6d
haowei <212410061@0365.tku.edu.tw>
2024-10-23 20:19:31 +0800

    add BlogSupaPage_61

--- src/App_61.jsx
+++ src/App_61.jsx
@@ -1,6 +1,7 @@
 import BlogStaticPage_61 from './pages/BlogStaticPage_61'
 import BlogNodePage_61 from './pages/BlogNodePage_61'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import BlogSupaPage_61 from './pages/BlogSupaPage_61'
 import './index.css'
 const App_61 = () => {
   return (
@@ -8,6 +9,7 @@ const App_61 = () => {
       <Routes>
         <Route path='/static_61' element={<BlogStaticPage_61 />} />
         <Route path='/node_61' element={<BlogNodePage_61 />} />
+        <Route path='/supa_61' element={<BlogSupaPage_61 />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/BlogSupaPage_61.jsx
@@ -0,0 +1,65 @@
+import '../index.css'
+import { FaMugSaucer } from 'react-icons/fa6'
+import { FaGlobe } from 'react-icons/fa6'
+import { useState, useEffect } from 'react'
+import { supabase } from '../db/clientSupabase'
+//useEffect:當組件被啟動後會useEffect會執行一次
+let api_url = 'http://localhost:5000/api/blog_61'
+const BlogSupaPage_61 = () => {
+  const [name, setName] = useState('周浩偉')
+  const [id, setId] = useState(212410061)
+  const [blogs, setBlogs] = useState([])
+
+  async function fetchBlogsFromSupaServer() {
+    try {
+      let { data, error } = await supabase.from('blog_61').select('*')
+      //let response = await fetch(url)
+      // const data = await response.json()
+      console.log('supabase blogs', data)
+      setBlogs(data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    fetchBlogsFromSupaServer(api_url)
+  }, [])
+
+  return (
+    <section className='blogs'>
+      <div className='section-title'>
+        <h2>latest blogs from Node -- </h2>
+        <h2 style={{ fontSize: '1.2rem', marginBottom: '1rem' }}>
+          {name} {id}
+        </h2>
+      </div>
+      <div className='blogs-center'>
+        {blogs?.map((blog) => {
+          const { id, title, category, img, description } = blog
+          return (
+            <article className='blog' key={id}>
+              <img src={img} alt='Coffee photo' className='img blog-img' />
+              <div className='blog-content'>
+                <span>
+                  {category}{' '}
+                  {category === 'lifestyle' ? (
+                    <FaMugSaucer />
+                  ) : category === 'travel' ? (
+                    <FaGlobe />
+                  ) : (
+                    ''
+                  )}
+                </span>
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <a href='#'>read more</a>
+              </div>
+            </article>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
+export default BlogSupaPage_61
